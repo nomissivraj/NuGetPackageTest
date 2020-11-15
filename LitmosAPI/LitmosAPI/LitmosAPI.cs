dd9e697d69f9732ec03bd42acfb7f6fe33a75167
nomissivraj <simonpeterjarvis@gmail.com>
2020-11-15 16:26:56 +0000

    Add project files.

---
+++ LitmosAPI/LitmosAPI/LitmosAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PP_lLitmosAPI.LitmosAPI
+{
+    class LitmosAPI
+    {
+        // Returns json string (if were real)
+        public string LitmosStuff()
+        {
+
+            string result = string.Empty;
+
+            int num = RandomNumber(0, 2);
+            switch (num)
+            {
+                case 0:
+                    result = "First";
+
+                    break;
+                case 1:
+                    result = "Second";
+
+                    break;
+                case 2:
+                    result = "Third";
+
+                    break;
+                default:
+                    result = "None";
+
+                    break;
+            }
+
+            return result;
+
+        }
+
+        Random _random = new Random();
+
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+    }
+
+}
